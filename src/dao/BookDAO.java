1cc0f250e0a040dfea9992d00e533f9e6eb36e96
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-10-04 10:12:28 +0530

    book by id method added

--- src/dao/BookDAO.java
+++ src/dao/BookDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import exception.BookException;
 import model.Book;
 
 public interface BookDAO {
@@ -7,5 +8,7 @@ public interface BookDAO {
 	public String addBook(Book book);
 	
 	public String removeBook(int id);
+	
+	public Book bookByID(int id) throws BookException;
 
 }
